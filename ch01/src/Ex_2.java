1e3b249ed6071f3bd2ef6d4d40b27ecb636eaada
Minuk Park <parkminuk0203@gmail.com>
2022-03-28 01:59:04 +0900

    print 와 println의 차이, 연산

---
+++ ch01/src/Ex_2.java
@@ -0,0 +1,30 @@
+
+public class Ex_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//print() - Ãâ·Â ÈÄ¿¡ ÁÙ¹Ù²Þ X
+		//println() - Ãâ·Â ÈÄ¿¡ ÁÙ¹Ù²Þ O
+		
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		
+		System.out.print("Hello");
+		System.out.print("Hello");
+		System.out.println("Hello");
+		
+		System.out.println(5+3); // µ¡¼À
+		System.out.println(5-3); // »¬¼À
+		System.out.println(5*3); // °ö¼À
+		System.out.println(5/3); // ³ª´°¼À
+		System.out.println(5/3.0); // ³ª´°¼À
+		
+		System.out.print(5+3); // µ¡¼À
+		System.out.print(5-3); // »¬¼À
+		System.out.print(5*3); // °ö¼À
+		System.out.print(5/3); // ³ª´°¼À
+		
+	}
+
+}
