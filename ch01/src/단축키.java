4cc45381ef7534bbdeb427655efc94ef160842ed
Minuk Park <parkminuk0203@gmail.com>
2022-03-28 01:50:15 +0900

    단축키

---
+++ ch01/src/단축키.java
@@ -0,0 +1,17 @@
+
+public class 단축키 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ctrl + shift + L 단축키 전체 목록 보기
+		//ctrl + shift + +,- 폰트 크기 증가,감소
+		//ctrl + D 한 줄 삭제
+		//ctrl + alt + shift + down 행단위 복사
+		//alt + shift + a 멀티컬럼 편집
+		//alt + up,down 행단위 이동
+		//ctrl + i 자동 들여쓰기
+		//ctrl + / 주석 (토글)
+		//ctrl + space 자동완성
+	}
+
+}
