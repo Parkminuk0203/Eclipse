0ebd5ccdf257016e973fe0c0d921f7cc17731c68
Minuk Park <parkminuk0203@gmail.com>
2022-03-28 02:05:39 +0900

    변수의 값 읽어오기

---
+++ ch02/src/VarEx1.java
@@ -0,0 +1,18 @@
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 4, y = 2;
+		System.out.println(x+y); // µ¡¼À
+		System.out.println(x-y); // »¬¼À
+		System.out.println(x*y); // °ö¼À
+		System.out.println(x/y); // ³ª´°¼À
+		
+		System.out.println(4+2); // µ¡¼À
+		System.out.println(4-2); // »¬¼À
+		System.out.println(4*2); // °ö¼À
+		System.out.println(4/2); // ³ª´°¼À
+	}
+
+}
