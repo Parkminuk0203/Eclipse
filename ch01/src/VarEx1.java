012e7c037b30c7216710d241eedca6afb0271ffb
Minuk Park <parkminuk0203@gmail.com>
2022-03-28 19:13:46 +0900

    변수, 상수, 리터럴 변수와 상수의 차이점에 대해서 배움

--- ch01/src/VarEx1.java
+++ ch01/src/VarEx1.java
@@ -1,5 +1,5 @@
 
-public class Ex_2 {
+public class VarEx1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ ch03/src/이론.java
@@ -0,0 +1,22 @@
+
+public class 이론 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 변수: 하나의 값을 저장하기 위한 공간 (몇번이고 값을 변경 O)
+		// 상수: 한 번만 값을 저장하기 위한 변수 (값을 변경 X)
+		// 리터럴: 그 자체로 값을 의미하는 것 = 기존의 상수, 자바에서 상수를 위와 같이 정의하여 리터럴이라는 단어를 씀
+		
+		//변수
+		// Ex1) int score = 100; , score = 100
+		//	        score = 200; , score = 200
+		
+		//상수
+		// [변수 앞에 'final' 키워드를 붙여야 함]
+		// Ex2) final int Max = 100; , Max는 상수 , Max = 100
+		//                Max = 200; , Error
+		
+		//ctrl + / 주석처리로 상수 선언하기전에 초기화 가능
+	}
+
+}
