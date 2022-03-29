127e7b47eb2bde5d6ec83a0f26e72204dc6eb250
Minuk Park <parkminuk0203@gmail.com>
2022-03-29 14:33:35 +0900

    Test

--- ch01/src/Hello.java
+++ ch01/src/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World");
+		
+		int x = 10;
+			x = 20;
+		System.out.println(x);
 	}
 
 }
