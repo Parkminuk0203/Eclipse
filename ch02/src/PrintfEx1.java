b026296d6710a163f3261d8fe4fdad0225c70b79
Minuk Park <parkminuk0203@gmail.com>
2022-04-03 01:12:51 +0900

    printf를 이용한 출력

--- ch02/src/PrintfEx1.java
+++ ch02/src/PrintfEx1.java
@@ -6,41 +6,41 @@ public class PrintfEx1 {
 		System.out.println(10/3);
 		System.out.printf("age:%d year:%d\n", 14, 2017);
 		System.out.println(10.0/3);
-		
+
 		System.out.printf("%d%n", 15); // 15
 		System.out.printf("%o%n", 15); // 17
 		System.out.printf("%x%n", 15); // f
 		System.out.printf("%X%n", 15); // F
 
-		
+
 		System.out.printf("%d%n", 15); // 15
 		System.out.printf("%#o%n", 15); // 017
 		System.out.printf("%#x%n", 15); // 0xf
 		System.out.printf("%#X%n", 15); // 0Xf
 		System.out.printf("%s%n", Integer.toBinaryString(15)); // 1111
-		
+
 		float f = 123.456789f;
-		System.out.printf("%f%n", f); // 123.456787, fÀÇ Á¤¹Ğµµ´Â 6ÀÚ¸®
+		System.out.printf("%f%n", f); // 123.456787, fì˜ ì •ë°€ë„ëŠ” 6ìë¦¬
 		double f1 = 123.456789;
-		System.out.printf("%f%n", f1); // 123.456787, dÀÇ Á¤¹Ğµµ´Â 15ÀÚ¸®
+		System.out.printf("%f%n", f1); // 123.456787, dì˜ ì •ë°€ë„ëŠ” 15ìë¦¬
 		System.out.printf("%e%n", f1); // 1.234568e+02
 		System.out.printf("%g%n", f1); // 123.457
 
-		System.out.printf("[%5d]%n", 10);  // [   10], 5ÀÚ¸®¸¸ Ç¥Çö
-		System.out.printf("[%-5d]%n", 10); // [10   ], -´Â ¿ŞÂÊ Á¤·Ä
-		System.out.printf("[05d]%n", 10);  // [00010], °ø¹é ´ë½Å 0À» Ã¤¿ò
-		System.out.printf("[%5d]%n", 1234567);  // [1234567], 5ÀÚ¸®¸¸ Ç¥ÇöÀÌÁö¸¸ 7ÀÚ¸® ¸ğµÎ Ãâ·ÂÇÔ
-		
+		System.out.printf("[%5d]%n", 10);  // [   10], 5ìë¦¬ë§Œ í‘œí˜„
+		System.out.printf("[%-5d]%n", 10); // [10   ], -ëŠ” ì™¼ìª½ ì •ë ¬
+		System.out.printf("[05d]%n", 10);  // [00010], ê³µë°± ëŒ€ì‹  0ì„ ì±„ì›€
+		System.out.printf("[%5d]%n", 1234567);  // [1234567], 5ìë¦¬ë§Œ í‘œí˜„ì´ì§€ë§Œ 7ìë¦¬ ëª¨ë‘ ì¶œë ¥í•¨
+
 		double d = 1.23456789;
 		System.out.printf("%f%n", d); // 1.234568
 		System.out.printf("%14.10f%n", d); // [  1.2345678900]
 		System.out.printf("%.6f%n", d); // 1.234568
-		
+
 		System.out.printf("[%s]%n", "www.codechobo.com"); 	 // [www.codechobo.com]
 		System.out.printf("[%20s]%n", "www.codechobo.com");  // [   www.codechobo.com]
 		System.out.printf("[%-20s]%n", "www.codechobo.com"); // [www.codechobo.com   ]
 		System.out.printf("[%.10s]%n", "www.codechobo.com"); // [www.codech]
-		
+
 	}
 	
 }
