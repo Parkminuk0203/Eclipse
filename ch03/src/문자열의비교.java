bd41fa32c9b7bd434780abe1ef0c66703b85c95d
Minuk Park <parkminuk0203@gmail.com>
2022-04-03 01:39:47 +0900

    문자열의 비교

---
+++ ch03/src/문자열의비교.java
@@ -0,0 +1,20 @@
+
+public class 문자열의비교 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "abc";
+		String str2 = "abc";
+
+		System.out.println(str1==str2); // true
+		System.out.println(str1.equals(str2)); // true
+				
+		String str3 = new String("abc");
+		String str4 = new String("abc");
+				
+		System.out.println(str3==str4); // false가 나온다
+		System.out.println(str3.equals(str4)); // true
+		// 위처럼 false가 나오기 때문에 문자열 비교에는 ==대신 equals()를 사용
+	}
+
+}
