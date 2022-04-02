3256c0574e5b6328e94515cad42912c2620272f6
Minuk Park <parkminuk0203@gmail.com>
2022-04-02 23:54:52 +0900

    반올림 Math.round(), 나머지 연산자

---
+++ ch03/src/Ex3_11.java
@@ -0,0 +1,34 @@
+
+public class Ex3_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long result = Math.round(4.52); // result에 5가 저장된다
+		
+		double pi = 3.141592;
+		
+		System.out.println(result); // 5
+		System.out.println(pi); // 3.141592
+		System.out.println(pi * 1000); // 3141.592
+		System.out.println(Math.round(pi*1000)); // 3142
+		
+		System.out.println(Math.round(pi*1000)/1000); 
+		// int(3142) / int(1000) 
+		// 3142 / 1000
+		// 3
+		
+		System.out.println(Math.round(pi*1000)/1000.0); // 3.142
+		// int(3142) / double(1000.0)
+		// 3142.0 / 1000.0
+		// 3.142
+		
+		System.out.println((double)Math.round(pi*1000)/1000); // 3.1424
+		
+		double shortPi = Math.round(pi * 1000) / 1000.0;
+		// Math.round(3.141592 * 1000) / 1000.0
+		// Math.round(3141.592) / 1000.0
+		// 3142 / 1000.0
+		System.out.println(shortPi); // 3.142
+	}
+
+}
---
+++ ch03/src/Ex3_12.java
@@ -0,0 +1,18 @@
+
+public class Ex3_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 8;
+		
+		System.out.printf("%d을 %d로 나누면, %n", x, y);
+		System.out.printf("몫은 %d이고, 나머지는 %d입니다.%n", x / y, x % y);
+		// 10을 8로 나누면, 
+		// 몫은 1이고, 나머지는 2입니다. 출력
+		
+		System.out.println(10 % 8); // 10을 8로 나눈 나머지 2가 출력됨
+		System.out.println(10 % -8); // 위와 동일
+	}
+
+}
