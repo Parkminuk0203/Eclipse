b772ac2414adf9041264f88227a95979bc4f1c52
Minuk Park <parkminuk0203@gmail.com>
2022-04-03 01:20:25 +0900

    int x,y 값을 저장하여 계산

--- ch02/src/VarEx2.java
+++ ch02/src/VarEx2.java
@@ -4,15 +4,15 @@ public class VarEx2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x = 4, y = 2;
-		System.out.println(x+y); // µ¡¼À
-		System.out.println(x-y); // »¬¼À
-		System.out.println(x*y); // °ö¼À
-		System.out.println(x/y); // ³ª´°¼À
+		System.out.println(x+y); // ë§ì…ˆ
+		System.out.println(x-y); // ëº„ì…ˆ
+		System.out.println(x*y); // ê³±ì…ˆ
+		System.out.println(x/y); // ë‚˜ëˆ—ì…ˆ
 		
-		System.out.println(4+2); // µ¡¼À
-		System.out.println(4-2); // »¬¼À
-		System.out.println(4*2); // °ö¼À
-		System.out.println(4/2); // ³ª´°¼À
+		System.out.println(4+2); // ë§ì…ˆ
+		System.out.println(4-2); // ëº„ì…ˆ
+		System.out.println(4*2); // ê³±ì…ˆ
+		System.out.println(4/2); // ë‚˜ëˆ—ì…ˆ
 	}
 
 }
