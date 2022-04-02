a08031175b9cc91f4159926e739c49f0dc8c30d5
Minuk Park <parkminuk0203@gmail.com>
2022-04-02 20:29:44 +0900

    타입간의 변환방법

---
+++ ch02/src/Ex_12.java
@@ -0,0 +1,16 @@
+
+public class Ex_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "3";
+		
+		System.out.println(str.charAt(0) - '0'); // 숫자 3
+		System.out.println('3' - '0' + 1); // 3 + 1 = 숫자 4
+		System.out.println(Integer.parseInt(str) + 1); // 3 + 1 = 숫자 4
+		System.out.println("3" + 1); // "3" + "1", 문자 31
+		System.out.println(3 + '0'); // '0'은 숫자로 48, 3 + 48 = 숫자 51
+		System.out.println((char)(3 + '0')); // 문자 3
+	}
+
+}
