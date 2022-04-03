54f5a38887697eb481deb16407b0c3df77ed5249
Minuk Park <parkminuk0203@gmail.com>
2022-04-03 15:05:54 +0900

    for문

---
+++ ch04/src/Ex4_8.java
@@ -0,0 +1,11 @@
+
+public class Ex4_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 3; i++) { // i= 1,2,3, 괄호{}안의 문장을 3번 반복
+			System.out.println("Hello");
+		}
+	}
+
+}
