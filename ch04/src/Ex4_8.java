766f9938b95b18d52648b06261de334d9369499e
Minuk Park <parkminuk0203@gmail.com>
2022-04-03 15:10:00 +0900

    for문

--- ch04/src/Ex4_8.java
+++ ch04/src/Ex4_8.java
@@ -6,6 +6,9 @@ public class Ex4_8 {
 		for (int i = 1; i <= 3; i++) { // i= 1,2,3, 괄호{}안의 문장을 3번 반복
 			System.out.println("Hello");
 		}
+		for (int i = 10; i >= 3; i--) { // i= 1~10
+			System.out.println(i);
+		}
 	}
 
 }
