84209dc842642160d05bbe7a0b5b1d20658548e2
Minuk Park <parkminuk0203@gmail.com>
2022-04-03 15:17:59 +0900

    for문

--- ch04/src/Ex4_8.java
+++ ch04/src/Ex4_8.java
@@ -9,6 +9,10 @@ public class Ex4_8 {
 		for (int i = 10; i >= 3; i--) { // i= 1~10
 			System.out.println(i);
 		}
+		for (int i = 1, j = 10; i <= 10; i++, j--) { // i는 for문 안에서만 선언되었기 때문에 밖에선 실행 불가
+			System.out.println("i="+i+"j="+j);
+		}
+//		System.out.println(i); // 선언된 i가 없어서 출력 불가
 	}
 
 }
