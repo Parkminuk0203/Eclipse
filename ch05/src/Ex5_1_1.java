1b47360a9d7f1e739e1d859034b65d20a6a13003
Minuk Park <parkminuk0203@gmail.com>
2022-04-10 00:26:08 +0900

    배열의 길이

---
+++ ch05/src/Ex5_1_1.java
@@ -0,0 +1,13 @@
+
+public class Ex5_1_1 {
+
+	public static void main(String[] args) {
+		//inedx범위 : 0~9
+		int[] arr = new int[10]; //길이가 5인 int배열 arr을 생성
+		System.out.println("arr.length="+arr.length);
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("arr["+i+"]="+arr[i]);
+		} //end of for
+	} //end of main
+} //end of class
