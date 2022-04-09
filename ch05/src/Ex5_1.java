ab6b5d43b35e2e9000a60d8b2c5bb02410836e60
Minuk Park <parkminuk0203@gmail.com>
2022-04-09 23:58:17 +0900

    배열의 생성과 선언, 배열의 인덱스

--- ch05/src/Ex5_1.java
+++ ch05/src/Ex5_1.java
@@ -1,31 +1,18 @@
-import java.util.Arrays;  // Arrays.toString()을 사용하기 위해 추가
-
 class Ex5_1 {
 	public static void main(String[] args) {
-		int[] iArr1 = new int[10];
-		int[] iArr2 = new int[10];
-//		int[] iArr3 = new int[]{100, 95, 80, 70, 60};
-		int[] iArr3 = {100, 95, 80, 70, 60};
-		char[] chArr = {'a', 'b', 'c', 'd'};
-
-		for (int i=0; i < iArr1.length ; i++ ) {
-			iArr1[i] = i + 1; // 1~10의 숫자를 순서대로 배열에 넣는다.
-		}
-
-		for (int i=0; i < iArr2.length ; i++ ) {
-			iArr2[i] = (int)(Math.random()*10) + 1; // 1~10의 값을 배열에 저장
-		}
-
-		// 배열에 저장된 값들을 출력한다.
-		for(int i=0; i < iArr1.length;i++) {
-			System.out.print(iArr1[i]+",");	
-		}
-		System.out.println();
-
-		System.out.println(Arrays.toString(iArr2));
-		System.out.println(Arrays.toString(iArr3));
-		System.out.println(Arrays.toString(chArr));
-		System.out.println(iArr3);
-		System.out.println(chArr);
-	}
-}
+//		int[] socre; 		// 1.배열 score를 선언(참조변수)
+//		socre = new int[5]; // 2.배열의 생성(int 저장공간 x 5)
+		
+		int[] score = new int[5]; //배열의 선언과 생성을 동시에
+		score[3] = 100; 
+		
+		System.out.println("socre[0]=" + score[0]);
+		System.out.println("socre[1]=" + score[1]);
+		System.out.println("socre[2]=" + score[2]);
+		System.out.println("socre[3]=" + score[3]);
+		System.out.println("socre[4]=" + score[4]);
+		
+		int value = score[3];
+		System.out.println("value="+value);
+	} //end of main
+} //end of class
