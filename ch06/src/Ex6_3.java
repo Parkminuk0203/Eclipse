6914be5f596804ded094a3b9906a95ece22440df
Minuk Park <parkminuk0203@gmail.com>
2022-04-12 04:23:12 +0900

    클래스 변수와 인스턴스 변수

--- ch06/src/Ex6_3.java
+++ ch06/src/Ex6_3.java
@@ -18,7 +18,7 @@ class Ex6_3 {
 		System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
 		//c1의 width와 height를 각각 50, 80으로 변경합니다.
 		
-		Card.width = 50; //c1.witdth=50;로 하면 iv랑 헷갈릴 수 있어서 클래스 이름을 권장.
+		Card.width = 50; //c1.width=50;로 하면 iv랑 헷갈릴 수 있어서 클래스 이름을 권장.
 		Card.height = 80;//c2.height=80;로 하면 iv랑 헷갈릴 수 있어서 클래스 이름을 권장.
 
 		System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
--- ch06/src/TEST.java
+++ ch06/src/TEST.java
@@ -1,14 +1,2 @@
-
-public class TEST {
-
-	public static void main(String[] args) {
-		
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=10;j++) {
-				System.out.printf("*");
-		}
-			System.out.println();
-		}
-		// 중첩 for문 사용
-	}
+class MyMath2 {
 }
