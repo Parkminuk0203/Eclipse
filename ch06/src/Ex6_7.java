d7a4dc4527db3e2655904d498d8a512040c23648
Minuk Park <parkminuk0203@gmail.com>
2022-04-11 20:39:29 +0900

    참조형 반환타입

---
+++ ch06/src/Ex6_7.java
@@ -0,0 +1,18 @@
+class Data2 { int x; }
+
+class Ex6_7 {
+	public static void main(String[] args) {
+		Data2 d = new Data2();
+		d.x = 10;
+		System.out.println("main() : x = " + d.x); //main() : x = 10
+
+		change(d);
+		System.out.println("After change(d)"); //After change(d)
+		System.out.println("main() : x = " + d.x); //main() : x = 1000
+	}
+
+	static void change(Data2 d) { //참조형 매개변수 (읽고 쓰기 가능)
+		d.x = 1000;
+		System.out.println("change() : x = " + d.x); //change() : x = 1000
+	}
+}
