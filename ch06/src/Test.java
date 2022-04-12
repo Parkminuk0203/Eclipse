9303aa6db7941d46d034be49a2fe460e2e1a5030
Minuk Park <parkminuk0203@gmail.com>
2022-04-13 01:27:48 +0900

    윤년 평년

---
+++ ch06/src/Test.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		
+	Scanner s = new Scanner(System.in);
+	System.out.println("년도를 입력하세요: ");
+	int year = s.nextInt();
+	if	((year % 4 == 0) && (year % 100 != 0) || year % 400 == 0 )
+		{
+		System.out.println("윤년입니다.");
+		} //end of if
+	else if (year % 100 == 0) {
+		System.out.println("평년입니다.");
+	} //end of else if문
+	else {
+		System.out.println("윤년이 아닙니다.");
+	} //end of else
+	
+	} //end of main
+} //end of class
