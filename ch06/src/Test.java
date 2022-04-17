19d11665e4a90eded41e41a27d4fac96f5923c10
Minuk Park <parkminuk0203@gmail.com>
2022-04-18 05:51:18 +0900

    윤년

--- ch06/src/Test.java
+++ ch06/src/Test.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Test {
 	public static void main(String[] args) {
 		
-	Scanner s = new Scanner(System.in);
+	Scanner s = new Scanner(System.in); // 화면에서 입력받음
 	System.out.println("년도를 입력하세요: ");
 	int year = s.nextInt();
 	if	((year % 4 == 0) && (year % 100 != 0) || year % 400 == 0 )
