91fea6d40170daa99fd73033b064b0a6ebbcdf73
Minuk Park <parkminuk0203@gmail.com>
2022-05-09 19:31:37 +0900

    객체의 생성과 사용

--- ch06/src/Ex6_2.java
+++ ch06/src/Ex6_2.java
@@ -1,7 +1,7 @@
 public class Ex6_2 {
 	public static void main(String[] args) {
-		Tv t1 = new Tv(); // Tv t1; t1 = new Tv();를 한 문장으로 가능
-		Tv t2 = new Tv(); // Tv t2; t2 = new Tv();를 한 문장으로 가능
+		Tv t1 = new Tv(); //Tv t1; t1 = new Tv();를 한 문장으로 가능
+		Tv t2 = new Tv(); //Tv t2; t2 = new Tv();를 한 문장으로 가능
 		
 		System.out.println("t1의 channel값은 " + t1.channel + "입니다."); 
 //		t1의 channel값은 0입니다.
@@ -17,11 +17,11 @@ public class Ex6_2 {
 		System.out.println("t2의 channel값은 " + t2.channel + "입니다."); 
 //		t2의 channel값은 0입니다.
 		
-		t2 = t1; // 참조변수 t1의 값을 t2의 저장.
+		t2 = t1; //참조변수 t1의 값을 t2의 저장.
 		
 		System.out.println("t1의 channel값은 " + t1.channel + "입니다."); 
 //		t1의 channel값은 7입니다.
 		System.out.println("t2의 channel값은 " + t2.channel + "입니다."); 
 //		t2의 channel값은 7입니다.
-	} // end of main의            
-} // end of class
+	} //end of main의            
+} //end of class
