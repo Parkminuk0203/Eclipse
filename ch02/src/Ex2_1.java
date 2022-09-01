76436ae46aefa1cba4f898b0166492021c3ca17b
Minuk Park <parkminuk0203@gmail.com>
2022-09-01 20:00:56 +0900

    화면에 글자 출력하기

--- ch02/src/Ex2_1.java
+++ ch02/src/Ex2_1.java
@@ -2,12 +2,14 @@
 public class Ex2_1 {
 
 	public static void main(String[] args) {
+//		화면에 Hello, world를 출력하고 줄바꿈을 한다.
 		System.out.println("Hello, Wolrd");
-//		Hello, Wolrd
-		System.out.println("Hello");
-//		Hello
+//		Hello, World
+//		화면에 Hello를 출력하고 줄바꿈 안한다
+		System.out.print("Hello");
+//		화면에 World를 출력하고 줄바꿈 한다.
 		System.out.println("World");
-//		World
+//		HelloWorld
 	}
 
 }
