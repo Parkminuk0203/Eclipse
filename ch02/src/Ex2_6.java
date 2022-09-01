c20bbd183bd02ad24731ca4d358c2c97ff84e14c
Minuk Park <parkminuk0203@gmail.com>
2022-09-01 20:16:09 +0900

    변수의 타입

--- ch02/src/Ex2_6.java
+++ ch02/src/Ex2_6.java
@@ -2,10 +2,16 @@
 public class Ex2_6 {
 
 	public static void main(String[] args) {
-		int x = 100; // x에 저장할 변수의 타입은 int로 한다.
-		double pi = 3.14; // x에 실수를 저장할 변수의 타입은 double로 한다.
-		char ch = 'a'; // 문자(1개)를 저장할 변수의 타입은 char로 한다.
-		String str = "abc"; // 여러 문자를 저장할 변수의 타입은 String
+//		정수타입 변수 x에 100을 대입
+		int x = 100;
+//		실수타입 변수 pi에 3.14를 대입
+		double pi = 3.14;
+//		문자 1개를 저장할 변수의 타입을 char로 한다.
+//		변수 ch에 문자 a를 대입
+		char ch = 'a';
+//		문자 여러개를 저장할 변수의 타입을 String으로 한다.
+//		변수 str에 문자열 abc를 대입
+		String str = "abc";
 		
 		System.out.println(x);
 //		100
