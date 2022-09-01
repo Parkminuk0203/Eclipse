67e8cd9e1aa5d39d3277555f664626f35f06aa71
Minuk Park <parkminuk0203@gmail.com>
2022-09-01 20:41:16 +0900

    두 변수의 값 바꾸기

--- ch02/src/Ex2_8.java
+++ ch02/src/Ex2_8.java
@@ -2,15 +2,18 @@
 public class Ex2_8 {
 
 	public static void main(String[] args) {
-		int x = 10, y =5; // int x = 10, int y = 5;를 한 줄로
+		int x = 10, y = 5;
+//		int x = 10; int y = 5;를 한 줄로도 가능
 		System.out.println("x=" + x);
 //		x=10
 		System.out.println("y=" + y);
 //		y=5
-		
-		int tmp = x; // 1. x의 값을 tmp에 저장
-		x = y;       // 2. y의 값을 x에 저장
-		y = tmp;     // 3. tmp에 저장된 값을 y에 저장
+		int tmp = x;
+//		정수타입 변수 tmp에 x의 값을 대입
+		x = y;
+//		y의 값을 x에 저장
+		y = tmp;
+//		tmp의 값을 y에 저장
 		System.out.println("x=" + x);
 //		x=5
 		System.out.println("y=" + y);
