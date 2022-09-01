5344d2a9429d7508bb0962e4c0418a8e5c3d17e3
Minuk Park <parkminuk0203@gmail.com>
2022-09-01 20:08:55 +0900

    변수의 선언과 저장

--- ch02/src/Ex2_4.java
+++ ch02/src/Ex2_4.java
@@ -2,11 +2,14 @@
 public class Ex2_4 {
 
 	public static void main(String[] args) {
-		int x = 5;
-		System.out.println(x);
-//		5
-		x = 10;
-		System.out.println(x);
-//      10
+//	5를 정수타입인 x에 대입
+	int x = 5; 
+	System.out.println(x);
+//	5
+	
+//	변수 x에 10을 대입, 기존에 5는 지워짐
+	x = 10;
+	System.out.println(x);
+//	10
 	}
 }
