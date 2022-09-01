a72cb290c2f6520202a52a224a37d8d840e28bc6
Minuk Park <parkminuk0203@gmail.com>
2022-09-01 20:33:07 +0900

    문자열 결합

--- ch02/src/Ex2_7.java
+++ ch02/src/Ex2_7.java
@@ -10,16 +10,25 @@ public class Ex2_7 {
 		System.out.println(str);
 //		Java8.0
 		System.out.println(7 + " ");
-//		7 
+//		7
+//		문자열 7
 		System.out.println(" " + 7);
 //		 7
+//		문자열 7
+		System.out.println(7 + "");
+//		7
+//		문자열 7
 		System.out.println("" + 7);
 //		7
+//		문자열 7
 		System.out.println("" + "");
 //
+//		빈 문자열
 		System.out.println(7 + 7 + "");
 //		14
+//		7+7을 먼저 더하고 문자열 14로 변환
 		System.out.println("" + 7 + 7);
 //		77
+//		왼쪽에서 부터 식을 수행하기 때문에 문자열 77이 됨
 	}
 }
