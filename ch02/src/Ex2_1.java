65f63cae7a8bc0e95a82e5030c98ac78a5005680
Minuk Park <parkminuk0203@gmail.com>
2022-09-01 20:34:38 +0900

    화면에 글자 출력하기

--- ch02/src/Ex2_1.java
+++ ch02/src/Ex2_1.java
@@ -2,13 +2,13 @@
 public class Ex2_1 {
 
 	public static void main(String[] args) {
-//		화면에 Hello, world를 출력하고 줄바꿈을 한다.
 		System.out.println("Hello, Wolrd");
 //		Hello, World
-//		화면에 Hello를 출력하고 줄바꿈 안한다
+//		화면에 Hello, world를 출력하고 줄바꿈을 한다.
 		System.out.print("Hello");
-//		화면에 World를 출력하고 줄바꿈 한다.
+//		화면에 Hello를 출력하고 줄바꿈 안한다
 		System.out.println("World");
+//		화면에 World를 출력하고 줄바꿈 한다.
 //		HelloWorld
 	}
 
--- ch02/src/Ex2_2.java
+++ ch02/src/Ex2_2.java
@@ -2,13 +2,12 @@
 public class Ex2_2 {
 
 	public static void main(String[] args) {
-//		화면에 Hello, world 출력된다.
 		System.out.println("Hello, world");
 //		Hello, world
-//		화면에 3+5=를 출력하고 줄바꿈 안한다.
 		System.out.print("3+5=");
-//		화면에 8을 출력한다.
+//		화면에 3+5=를 출력하고 줄바꿈 안한다.
 		System.out.println(3+5);
+//		화면에 8을 출력한다.
 //		3+5=8
 	}
 
