a0341565773932ff32d85dc7f4c96fda5d8a2389
Minuk Park <parkminuk0203@gmail.com>
2022-09-16 15:17:36 +0900

    학교

--- UniverSity/src/D220916_2.java
+++ UniverSity/src/D220916_2.java
@@ -1,8 +1,9 @@
 
-public class D220916_1 {
+public class D220916_2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("I'm a new Java programmer.");
 	}
 
 }
