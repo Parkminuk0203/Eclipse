fe817b656c0ed14875c65c0679b035f8bad17eb2
Minuk Park <parkminuk0203@gmail.com>
2022-11-17 20:22:03 +0900

    기본 생성자

--- ch06/src/Ex6_11.java
+++ ch06/src/Ex6_11.java
@@ -6,7 +6,7 @@ class Data_1 {
 
 class Data_2 {
 	int value;
-//    Data_2() {} //기본 생성자. 없으면 에러남
+    Data_2() {} //기본 생성자. 없으면 에러남
 	Data_2(int x) {   // 매개변수가 있는 생성자.
 		value = x;
 	}
@@ -15,7 +15,7 @@ class Data_2 {
 class Ex6_11 {
 	public static void main(String[] args) {
 		Data_1 d1 = new Data_1();
-//		Data_2 d2 = new Data_2(); // compile error발생.
+		Data_2 d2 = new Data_2(); // compile error발생.
 		//The constructor Data_2() is undefined. // Data_2()의 생성자가 없다.
 	}
 }
